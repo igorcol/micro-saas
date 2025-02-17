50f68f62b2e1d131d509148e0a3889bef96898f4
igorcol <igor.colombini@gmail.com>
2025-02-16 21:22:07 -0300

    Primeiro commit do projeto

---
+++ src/app/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Main page when logged-in</h1>
+    )
+}
---
+++ src/services/auth/index.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "../database"
+ 
+export const { 
+  auth, 
+  handlers:{GET,POST}
+} = NextAuth({
+
+  pages: {
+    signIn: '/auth',
+    signOut: '/auth',
+    error: '/auth',
+    verifyRequest: '/auth',
+    newUser: '/app'
+  },
+
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER,
+        port: Number(process.env.EMAIL_PORT),
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      },
+      from: process.env.EMAIL_FROM
+    })
+  ],
+  secret: process.env.EMAIL_SECRET
+})
