354455bb334d1486131b7971bebd14033d0eb9b3
igorcol <igor.colombini@gmail.com>
2025-02-21 15:43:37 -0300

    padronização de nome dos componentes da dashboard

--- src/app/app/_components/main-sidebar.tsx
+++ src/app/app/_components/main-sidebar.tsx
@@ -1,64 +1,68 @@
-'use client'
+"use client";
 import {
-    Sidebar,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarHeaderTitle,
-    SidebarMain,
-    SidebarNav,
-    SidebarNavHeader,
-    SidebarNavHeaderTitle,
-    SidebarNavLink,
-    SidebarNavMain,
-  } from "@/components/dashboard/sidebar";
+  DashboardSidebar,
+  DashboardSidebarFooter,
+  DashboardSidebarHeader,
+  DashboardSidebarHeaderTitle,
+  DashboardSidebarMain,
+  DashboardSidebarNav,
+  DashboardSidebarNavHeader,
+  DashboardSidebarNavHeaderTitle,
+  DashboardSidebarNavLink,
+  DashboardSidebarNavMain,
+} from "@/components/dashboard/sidebar";
 import { usePathname } from "next/navigation";
-import { HomeIcon, MixerVerticalIcon} from '@radix-ui/react-icons'
+import { HomeIcon, MixerVerticalIcon } from "@radix-ui/react-icons";
 import { UserDropdown } from "./user-dropdown";
 import { Logo } from "@/components/logo";
 
 export function MainSidebar() {
+  const pathName = usePathname();
 
-    const pathName = usePathname()
+  const isActive = (path: string) => {
+    return pathName === path;
+  };
 
-    const isActive = (path: string) => {
-        return pathName === path
-    }
+  return (
+    <DashboardSidebar>
+      <DashboardSidebarHeader>
+        <Logo />
+      </DashboardSidebarHeader>
+      <DashboardSidebarMain className="flex flex-col flex-grow">
+        <DashboardSidebarNav>
+          <DashboardSidebarNavMain>
+            <DashboardSidebarNavLink href="/App" active={isActive("/App")}>
+              <HomeIcon className="w-3 h-3 mr-3" />
+              Tarefas
+            </DashboardSidebarNavLink>
+            <DashboardSidebarNavLink
+              href="/App/settings"
+              active={isActive("/App/settings")}
+            >
+              <MixerVerticalIcon className="w-3 h-3 mr-3" />
+              Configurações
+            </DashboardSidebarNavLink>
+          </DashboardSidebarNavMain>
+        </DashboardSidebarNav>
 
-    return (
-        <Sidebar>
-        <SidebarHeader>
-          <Logo/>
-        </SidebarHeader>
-        <SidebarMain className="flex flex-col flex-grow">
-          <SidebarNav>
-            <SidebarNavMain>
-              <SidebarNavLink href="/App" active={isActive('/App')}>
-                <HomeIcon className="w-3 h-3 mr-3" /> 
-                Tarefas
-              </SidebarNavLink>
-              <SidebarNavLink href="/App/settings" active={isActive('/App/settings')}>
-                <MixerVerticalIcon className="w-3 h-3 mr-3" /> 
-                Configurações
-              </SidebarNavLink>
-            </SidebarNavMain>
-          </SidebarNav>
+        <DashboardSidebarNav className="mt-auto">
+          <DashboardSidebarNavHeader>
+            <DashboardSidebarNavHeaderTitle>
+              Links Extras
+            </DashboardSidebarNavHeaderTitle>
+          </DashboardSidebarNavHeader>
+          <DashboardSidebarNavMain>
+            <DashboardSidebarNavLink href="/">
+              Precisa de Ajuda?
+            </DashboardSidebarNavLink>
+            <DashboardSidebarNavLink href="/">Site</DashboardSidebarNavLink>
+          </DashboardSidebarNavMain>
+        </DashboardSidebarNav>
+      </DashboardSidebarMain>
 
-          <SidebarNav className="mt-auto">
-            <SidebarNavHeader>
-                <SidebarNavHeaderTitle>
-                    Links Extras
-                </SidebarNavHeaderTitle>
-            </SidebarNavHeader>
-            <SidebarNavMain>
-              <SidebarNavLink href="/">Precisa de Ajuda?</SidebarNavLink>
-              <SidebarNavLink href="/">Site</SidebarNavLink>
-            </SidebarNavMain>
-          </SidebarNav>
-        </SidebarMain>
-
-        <SidebarFooter>
-            <UserDropdown/>
-        </SidebarFooter>
-      </Sidebar>
-    )
+      <DashboardSidebarFooter>
+        <UserDropdown />
+      </DashboardSidebarFooter>
+    </DashboardSidebar>
+  );
 }
--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,111 +1,134 @@
-
 // * ESTRUTURA SEGUINDO COMPOSITION PATTERN
 
-import Link from "next/link"
+import Link from "next/link";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export type SidebarGenericProps<T = any> = {
-    children: React.ReactNode
-    className?: string
-} & T
-
+export type DashboardSidebarGenericProps<T = any> = {
+  children: React.ReactNode;
+  className?: string;
+} & T;
 
 // * ESTRUTURA GERAL
-export function Sidebar({ children, className} : SidebarGenericProps) {
-    return (
-        <aside className={cn(['border-r border-border flex flex-col space-y-6'], className)} >
-            {children}
-        </aside>
-    )
+export function DashboardSidebar({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return (
+    <aside
+      className={cn(
+        ["border-r border-border flex flex-col space-y-6"],
+        className
+      )}
+    >
+      {children}
+    </aside>
+  );
 }
 
 // * CABEÇALHO
-export function SidebarHeader({ children, className} : SidebarGenericProps) {
-    return (
-        <header className={cn(['px-6 py-3 border-b border-border'], className)} >
-            {children}
-        </header>
-    )
+export function DashboardSidebarHeader({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return (
+    <header className={cn(["px-6 py-3 border-b border-border"], className)}>
+      {children}
+    </header>
+  );
 }
 
 // * TITULO DO CABEÇALHO
-export function SidebarHeaderTitle({ children, className} : SidebarGenericProps) {
-    return (
-        <h2 className={cn([''], className)} >
-            {children}
-        </h2>
-    )
+export function DashboardSidebarHeaderTitle({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return <h2 className={cn([""], className)}>{children}</h2>;
 }
 
 // * "DIV" PRINCIPAL PARA AS NAVS
-export function SidebarMain({ children, className} : SidebarGenericProps) {
-    return (
-        <main className={cn(['px-3'], className)} >
-            {children}
-        </main>
-    )
+export function DashboardSidebarMain({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return <main className={cn(["px-3"], className)}>{children}</main>;
 }
 
 // * ESTRUTURA DA NAV
-export function SidebarNav({ children, className} : SidebarGenericProps) {
-    return (
-        <nav className={cn([''], className)} >
-            {children}
-        </nav>
-    )
+export function DashboardSidebarNav({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return <nav className={cn([""], className)}>{children}</nav>;
 }
 
 // * CABEÇALHO DA NAV
-export function SidebarNavHeader({ children, className} : SidebarGenericProps) {
-    return (
-        <header className={cn([''], className)} >
-            {children}
-        </header>
-    )
+export function DashboardSidebarNavHeader({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return <header className={cn([""], className)}>{children}</header>;
 }
 
 // * TITULO DO CABEÇALHO
-export function SidebarNavHeaderTitle({ children, className} : SidebarGenericProps) {
-    return (
-        <div className={cn(['text-xs uppercase text-muted-foreground ml-3'], className)} >
-            {children}
-        </div>
-    )
+export function DashboardSidebarNavHeaderTitle({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return (
+    <div
+      className={cn(
+        ["text-xs uppercase text-muted-foreground ml-3"],
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 }
 
 // * CORPO PRINCIPAL DA NAV
-export function SidebarNavMain({ children, className} : SidebarGenericProps) {
-    return (
-        <main className={cn(['flex flex-col'], className)} >
-            {children}
-        </main>
-    )
+export function DashboardSidebarNavMain({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return <main className={cn(["flex flex-col"], className)}>{children}</main>;
 }
 
-type SidebarNavLinkProps = {
-    href: string
-    active?: boolean
-}
+type DashboardSidebarNavLinkProps = {
+  href: string;
+  active?: boolean;
+};
 
 // * LINKS
-export function SidebarNavLink({ children, className, href, active} : SidebarGenericProps<SidebarNavLinkProps>) {
-    return (
-        <Link href={href} className={cn(
-            ['flex items-center text-sm px-3 py-2 rounded-md'], 
-            active && 'bg-secondary',
-            className)
-        } >
-            {children}
-        </Link>
-    )
+export function DashboardSidebarNavLink({
+  children,
+  className,
+  href,
+  active,
+}: DashboardSidebarGenericProps<DashboardSidebarNavLinkProps>) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        ["flex items-center text-sm px-3 py-2 rounded-md"],
+        active && "bg-secondary",
+        className
+      )}
+    >
+      {children}
+    </Link>
+  );
 }
 
 // * FOOTER
-export function SidebarFooter({ children, className} : SidebarGenericProps) {
-    return (
-        <footer className={cn(['p-6 mt-auto border-t border-border'], className)} >
-            {children}
-        </footer>
-    )
+export function DashboardSidebarFooter({
+  children,
+  className,
+}: DashboardSidebarGenericProps) {
+  return (
+    <footer className={cn(["p-6 mt-auto border-t border-border"], className)}>
+      {children}
+    </footer>
+  );
 }
