9c66ac9e2414bde95f1448dd2f8eb8d01a0f7674
igorcol <igor.colombini@gmail.com>
2025-02-21 20:28:27 -0300

    PRISMA MIGRATE: add-done-at-on-task

---
+++ src/app/app/(main)/actions.ts
@@ -0,0 +1,16 @@
+import { auth } from "@/services/auth"
+import { prisma } from "@/services/database"
+
+export async function getUserTodos() {
+    // Verificar o usuario logado 
+    // Retornar suas taregas
+    const session = await auth()
+
+    const todos = await  prisma.todo.findMany({
+        where: {
+            userId: session?.user?.id
+        }
+    })
+
+    return []
+}
